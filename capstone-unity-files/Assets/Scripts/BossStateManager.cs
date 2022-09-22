38e1f98b47e6e67a582c1676f85d4f0345d1d6aa
Daniel Creighton <creightond7@gmail.com>
2022-09-21 21:45:22 -0400

    created boss fsm

---
+++ capstone-unity-files/Assets/Scripts/BossStateManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossStateManager : MonoBehaviour
+{
+    State currentState;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RunStateMachine();
+    }
+
+    private void RunStateMachine()
+    {
+        State nextState = currentState?.RunSurrentState();
+
+        if (nextState != null)
+        {
+            SwitchToNextState(nextState);
+        }
+    }
+
+    private void SwitchToNextState(State nextState)
+    {
+        currentState = nextState;
+    }
+}
