2af484cc1f3383e06759f7a9d7df1747dfd143b1
Daniel Creighton <creightond7@gmail.com>
2022-10-12 20:55:48 -0400

    Created manager script

---
+++ capstone-unity-files/Assets/Scripts/PersistentManagerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentManagerScript : MonoBehaviour
+{
+    public static PersistentManagerScript Instance { get; private set; }
+
+    //store data here
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
