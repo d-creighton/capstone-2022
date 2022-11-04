324928a1e8ea5605f2d5bac3ac1cd7a275814ff9
Daniel Creighton <creightond7@gmail.com>
2022-11-04 17:47:08 -0400

    added player health bar functionality

---
+++ capstone-unity-files/Assets/Scripts/PlayerProperties.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperties : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public BarFunctionality healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxSliderValue(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //damage code
+    }
+
+    void TakeDamge(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetValue(currentHealth);
+    }
+}
