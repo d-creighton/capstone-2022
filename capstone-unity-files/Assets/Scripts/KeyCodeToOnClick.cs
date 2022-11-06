deaa17d45fe0d9abaf25c0120e444d4206d56b7f
Daniel Creighton <creightond7@gmail.com>
2022-11-06 15:45:21 -0500

    menu navigation work

---
+++ capstone-unity-files/Assets/Scripts/KeyCodeToOnClick.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyCodeToOnClick : MonoBehaviour
+{
+    public KeyCode key;
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(key))
+        {
+            button.onClick.Invoke();
+        }
+    }
+}
