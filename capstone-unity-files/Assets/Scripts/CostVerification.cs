f7492dac4dcd182ffee08e2617604bb2a7f3f908
Daniel Creighton <creightond7@gmail.com>
2022-11-06 17:01:27 -0500

    fleshed out menu further

---
+++ capstone-unity-files/Assets/Scripts/CostVerification.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CostVerification : MonoBehaviour
+{
+    public int cost;
+    public bool canInteract;
+    public Button button;
+
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canInteract = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check atb amount
+
+        // set button interactable to true if desired amount reached
+    }
+}
