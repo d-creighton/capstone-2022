e41396b3ac1755e87b08f239d37607dc753afd45
Daniel Creighton <creightond7@gmail.com>
2022-11-19 17:01:50 -0500

    created framework for applying commands

---
+++ capstone-unity-files/Assets/Scripts/Combat Menu Scripts/CommandSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChooseCommand(int commandID)
+    {
+        //if 1 or 2, choose attack
+        //if 3 or 4, choose heal
+    }
+
+    public void Attack()
+    {
+        //if attack 1
+        //if attack 2
+    }
+
+    public void Heal()
+    {
+        //if item 1
+        //if item 2
+    }
+}
