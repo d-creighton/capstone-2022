f3906ef598746b568af28f87ab84483a15e2d18a
Daniel Creighton <creightond7@gmail.com>
2022-11-19 21:12:16 -0500

    merged with companion

--- capstone-unity-files/Assets/Scripts/Companion Scripts/CompanionIdleState.cs
+++ capstone-unity-files/Assets/Scripts/Companion Scripts/CompanionIdleState.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class CompanionIdleState : State
 {
     public SeekingState seekingState;
-    public bool commandReceived;
+    public bool commandReceived = false;
 
     public override State RunCurrentState()
     {
         // Await commands from player
         // Wander slightly
         // Cry semi-randomly
+            // enumerate seconds
+            // random number of seconds
+            // if just cried
+            // wait at least x number of seconds
+            // repeat
+
         // Dodge boss attacks
         // Execute small auto-attacks
 
