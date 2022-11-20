0471a79531953bd2d598297ffa78c597fbf6cba5
Daniel Creighton <creightond7@gmail.com>
2022-11-19 20:07:00 -0500

    Update CommandSystem.cs

--- capstone-unity-files/Assets/Scripts/Combat Menu Scripts/CommandSystem.cs
+++ capstone-unity-files/Assets/Scripts/Combat Menu Scripts/CommandSystem.cs
@@ -4,31 +4,43 @@ using UnityEngine;
 
 public class CommandSystem : MonoBehaviour
 {
+    public int attackChoice;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ChooseCommand(int commandID)
+/*    public void ChooseCommand(int commandID)
     {
         //if 1 or 2, choose attack
         //if 3 or 4, choose heal
+        if(commandID == 1 || 2)
+        {
+            Attack(commandID);
+        }
+        else
+        {
+            Heal(commandID);
+        }
     }
-
+*/
     public void Attack()
     {
+        int attackChoice;
         //if attack 1
+        if(attackChoice == 1)
+        {
+            //do something
+        }
         //if attack 2
+        else
+        {
+            //do something
+        }
     }
 
-    public void Heal()
+    public void Heal(int itemChoice)
     {
         //if item 1
         //if item 2
