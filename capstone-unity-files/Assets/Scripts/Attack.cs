ef4672e840961dd122e0373d6bf0157b6f51b871
Daniel Creighton <creightond7@gmail.com>
2022-11-21 19:02:09 -0500

    implemented taking damage

--- capstone-unity-files/Assets/Scripts/Attack.cs
+++ capstone-unity-files/Assets/Scripts/Attack.cs
@@ -7,6 +7,7 @@ public class Attack : MonoBehaviour
     //public bool canAttack = false;
     public CompanionAttackState attackChoice;
     public CompanionIdleState giveCommand;
+    public Properties targetRef;
     
     // Start is called before the first frame update
     void Start()
@@ -28,11 +29,15 @@ public class Attack : MonoBehaviour
 
     public void WeakAttack()
     {
-
+        //subtract 25 (1/4) health from target
+        targetRef.TakeDamage(25);
+        //spawn projectile in direction of target
     }
 
     public void StrongAttack()
     {
-
+        //subtract 50 (1/2) health from target
+        targetRef.TakeDamage(50);
+        //spawn projectile in direction of target
     }
 }
--- capstone-unity-files/Assets/Scripts/Shared Scripts/Properties.cs
+++ capstone-unity-files/Assets/Scripts/Shared Scripts/Properties.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Properties : MonoBehaviour
 {
+    public string unitName;
+    public int unitLevel;
+
+    public int maxHP;
+    public int currentHP;
+
+    public BarFunctionality healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHP = maxHP;
+        healthBar.SetMaxSliderValue(maxHP);
+        healthBar.SetLabel(currentHP, maxHP);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Call if projectile hits
+    public bool TakeDamage(int damageTaken)
     {
-        
+        // Take damage
+        currentHP -= damageTaken;
+
+        // Check if this unit has died
+        if(currentHP <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
